d34d28f5522e7656c1c79e36108cdd452d460435
Juan Alves <juanalves.estudo@gmail.com>
2025-11-09 00:06:03 -0300

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
-import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // 👈 ativa o Tailwind v4 no Vite
+    tailwindcss(),
   ],
-})
+  base: "/TOUR-2-FAVELA/",
+});
